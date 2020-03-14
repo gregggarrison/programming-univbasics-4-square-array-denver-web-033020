fd28e2eba2dcfeffab41954a4797dbedd34901a0
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-14 17:56:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,19 @@
-def square_array(numbers)
+def square_array(array)
+  new_array = []
+  counter = 0 
+  
+  while 
+  
+  end 
+  
+   #need a loop, iterates each element
+  
+  
+  
+  # incrment our counter after the loop
+  
+  
+  #return the new array 
   
 end
 
