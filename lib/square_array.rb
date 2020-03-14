005e7658900a927078c07933a982c152dd3bd3a9
Gregg Garrison <gregg.garrison@rocketmail.com>
2020-03-14 18:00:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,9 @@ def square_array(array)
   new_array = []
   counter = 0 
   
-  while 
-  
+  while counter < array.length do 
+    new_array << array[counter] **2 
+    counter += 1 
   end 
   
    #need a loop, iterates each element
